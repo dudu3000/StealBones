2cb00603b4d10c9ae4d3a3a90fd832a61de4d349
Ionut-Alexandru Duduman <dudumanionut96@gmail.com>
2024-09-28 23:08:40 +0300

    creating main menu scene
    Creating main menu scene:
    - Adding background
    - Adding characters on idle animation
    - Adding panel with title of the game
    - Adding panel with buttons

---
+++ Assets/Scripts/MainMenuSharkController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSharkController : MonoBehaviour
+{
+    private float speed = 5f;
+    private Vector3 initialPosition = new Vector3(-4, -0.5f, -4.8f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = initialPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (transform.position.x > 50 || transform.position.x < -50) {
+            transform.Rotate(Vector3.up * 180);
+        }
+    }
+}
