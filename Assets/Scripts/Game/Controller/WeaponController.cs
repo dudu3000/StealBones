74f7982ac73d320ee246c5d57a8b337716cec4b9
Ionut-Alexandru Duduman <dudumanionut96@gmail.com>
2024-10-03 14:03:20 +0300

    Adding pattern for bullets
    Adding pattern for how bullets will fly towards the haircross

--- Assets/Scripts/Game/Controller/WeaponController.cs
+++ Assets/Scripts/Game/Controller/WeaponController.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponController : WeaponControllerAbstract
+public class WeaponController : BaseWeaponController
 {
-    public ParticleSystem gunSmoke;
-    public GameObject bullet;
-    public GameObject aimLookAt;
-    private int numberOfBullets = 9;
-    private GameObject bulletSpawner;
-
     void Start()
     {
         isFiring = false;
-        reloadTime = 5f;
-        reloading = 0f;
+        reloadTime = 3f;
+        reloading = 5f;
         bulletSpawner = GameObject.Find("BulletSpawner");
         aimLookAt = GameObject.Find("AimLookAt");
     }
@@ -26,29 +20,6 @@ public class WeaponController : WeaponControllerAbstract
             reloading = 0f;
         } else {
             reloading += Time.deltaTime;
-        }
-    }
-    public override void StartFiring() {
-        isFiring = true;
-    }
-
-    public override void StopFiring()
-    {
-        isFiring = false;
-    }
-
-    public override void Firing() {
-        gunSmoke.Play();
-        isFiring = false;
-        SpawnBullets();
-    }
-
-    private void SpawnBullets() {
-        for (int i = 0; i < numberOfBullets; i++) {
-            aimLookAt = GameObject.Find("AimLookAt");
-            bulletSpawner = GameObject.Find("BulletSpawner");
-            GameObject bulletSpawned = Instantiate(bullet, bulletSpawner.transform.position, bulletSpawner.transform.rotation);
-            bulletSpawned.GetComponent<Rigidbody>().AddForce(aimLookAt.transform.position - bulletSpawner.transform.position, ForceMode.Impulse);
         }
     }
 }
